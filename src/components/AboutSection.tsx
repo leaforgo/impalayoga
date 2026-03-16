545f7b739e45c6adc15a0ff071b3dc32bf33e380
Mihály Orsós <ormraat.pte@gmail.com>
2026-03-16 16:29:55 +0100

    new_image

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import leaPortrait from "@/assets/lea-portrait.png";
+import leaPortrait from "@/assets/lea-croatia.jpg";
 
 const AboutSection = () => {
   return (
